09bf39d2352c767080d40f44ac6c6b86709a8c73
Oleksandr-HK <alexanderpeterburg21@gmail.com>
2025-12-20 22:30:04 +0100

    - Added debug for coverage

--- .github/workflows/check-coverage.sh
+++ .github/workflows/check-coverage.sh
@@ -8,10 +8,12 @@ coverage=$(jq -r '
   (.coverageOperationMap.counter.full / .coverageOperationMap.counter.all) * 100
 ' swagger-coverage-results.json)
 
+echo "Current coverage as per report ${coverage}"
+
 coverage=${coverage%.*} #
 
 if [ "$coverage" -lt $required_coverage ]; then
-  echo "Not enough API coverage! Current API code Coverage: ${coverage}%"
+  echo "Not enough API coverage! Current full API code Coverage: ${coverage}%"
   echo "Desired API coverage: ${required_coverage}%"
   exit 1
 fi
